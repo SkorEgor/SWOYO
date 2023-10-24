79d7932d475eef6a52dca2b3fcbae2f5088174bf
SkorEgor <92719975+SkorEgor@users.noreply.github.com>
2023-10-24 16:09:30 +0300

    tests for general statistics and file reading

--- test_text_stat.py
+++ test_text_stat.py
@@ -27,15 +27,17 @@ def test_finding_words_from_text(text: str, word_list: list):
     assert word_list_from_text(text) == word_list
 
 
+# Текст для разделения не содержит знаков
 def test_empty_text():
     assert word_list_from_text("") == []
 
 
 # Подсчет количества каждой буквы
 def test_counting_letters():
-    assert count_of_letter_uses(mixed_text.lower()) == letters_and_number_for_mixed_text
+    assert count_of_letter_uses(mixed_text.lower()) == number_of_each_letters_in_mixed_text
 
 
+# Количество букв в пустом тексте
 def test_counting_letters_in_empty_text():
     empty_dictionary = {}
     rus_alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
@@ -69,10 +71,28 @@ def test_number_of_paragraphs(text_with_paragraphs: str, count_paragraphs: int):
     assert number_of_paragraphs(text_with_paragraphs) == count_paragraphs
 
 
+# Параграфов в тексте из примера
 def test_number_of_paragraphs_in_text():
-    assert number_of_paragraphs(mixed_text) == 37
+    assert number_of_paragraphs(mixed_text) == paragraph_amount_in_mixed_text
 
 
 # Поиск слов с русскими и английскими буквами
 def test_bilingual_word():
     assert bilingual_word("sdsdfdf ffdfывыв, dffdf ввааав. dsdsddsdsф ававdsd 988989assыфы вывыв95895dsds") == 5
+
+
+# Частота использования буквы, доля слов с буквой
+def test_frequency_and_proportion_of_words_with_letter():
+    val = frequency_and_proportion_of_words_with_letter(word_list=words_from_mixed_text,
+                                                        letters_and_number=number_of_each_letters_in_mixed_text)
+    assert val == statistics_for_letter_from_mixed_text
+
+
+# Работа с файлом
+def test_working_with_file():
+    assert text_stat("text_for_test.txt") == resulting_statistics_for_mixed_text
+
+# Не существующий файл
+def test_non_existing_file():
+    assert text_stat("Hi cat") == {'error': 'Файл не найден'}
+
